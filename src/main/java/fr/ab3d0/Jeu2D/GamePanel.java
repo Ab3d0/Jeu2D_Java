9eb4d2863c7c613bb30b69647d83530050fdb91f
Ab3d0 <a.bridja77@gmail.com>
2025-07-17 21:42:46 +0200

    feat: Add game loop and insert a character

--- src/main/java/fr/ab3d0/Jeu2D/GamePanel.java
+++ src/main/java/fr/ab3d0/Jeu2D/GamePanel.java
@@ -15,13 +15,22 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenCol; /* ça fait 760 pixels */
     final int screenHeight = tileSize * maxScreenRow; /* ça fait 576 pixels */
 
+    KeyBorder keyH = new KeyBorder();
+
+
     Thread gameThread;
+
+    int playerX = 100;
+    int playerY= 100;
+    int playerSpeed = 4;
 
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread(){
@@ -31,6 +40,42 @@ public class GamePanel extends JPanel implements Runnable{
 
     @Override
     public void run() {
+        while(gameThread != null){
+            update();
+            repaint();
+
+        }
+    }
+
+    public void update(){
+        if(keyH.upPressed == true){
+            playerY -= playerSpeed;
+
+        }else if(keyH.downPressed == true){
+            playerY += playerSpeed;
+        }
+        else if(keyH.leftPressed == true){
+            playerX -= playerSpeed;
+        }
+        else if (keyH.rightPressed == true){
+            playerX += playerSpeed;
+        }
+
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.white);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+
+        g2.dispose();
+
+
+
+
 
     }
 }
